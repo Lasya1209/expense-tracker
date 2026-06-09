c83978b480c59d677527e30db2665160f5f23f83
Lasya <klasya2002@gmail.com>
2026-06-09 11:54:39 +0530

    Added config code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api/transactions': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       },
       '/api/users': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
